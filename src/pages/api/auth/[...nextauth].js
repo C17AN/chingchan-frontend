73528e70eea99bf069d38a66c2d25e84d832bafd
김찬민 <chanmin@gimchanmin-ui-MacBookPro.local>
2023-04-06 16:59:31 +0900

    chore: 소셜 로그인 프로바이더설정을 추가한다

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,13 +1,21 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import KakaoProvider from "next-auth/providers/kakao";
+import GoogleProvider from "next-auth/providers/google";
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET,
     }),
-    // ...add more providers here
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
   ],
+  pages: {
+    signIn: "/auth/signIn",
+  },
 };
 export default NextAuth(authOptions);
